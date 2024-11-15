97c08dfba0da3f4530e3d009513dc6aea440fb44
lucasbezq <lucasbarrosez@gmail.com>
2024-11-15 16:02:45 -0300

    feat: extrair lógica dos handlers

--- router/routes.go
+++ router/routes.go
@@ -8,8 +8,8 @@ import (
 func initializeRoutes(router *gin.Engine) {
 	v1 := router.Group("/api/v1")
 	{
-		v1.POST("/opennings/oppening", handler.CreateOppeningHandler)
-		v1.GET("/opennings", handler.FindOppeningHandler)
+		v1.POST("/opennings", handler.CreateOppeningHandler)
+		v1.GET("/opennings/oppening", handler.FindOppeningHandler)
 		v1.PUT("/opennings", handler.UpdateOppeningHandler)
 		v1.DELETE("/opennings", handler.DeleteOppeningHandler)
 		v1.GET("/opennings", handler.ListOppeningHandler)
