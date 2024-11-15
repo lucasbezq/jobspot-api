f8e0ba956a17914abbff7d3b73516e84655d69b8
lucasbezq <lucasbarrosez@gmail.com>
2024-11-15 16:03:02 -0300

    feat: adicionar logs personalizados

--- main.go
+++ main.go
@@ -1,9 +1,22 @@
 package main
 
-import "github.com/lucasbezq/jobspot-api/router"
+import (
+	"github.com/lucasbezq/jobspot-api/config"
+	"github.com/lucasbezq/jobspot-api/router"
+)
+
+var (
+	logger *config.Logger
+)
 
 func main() {
+	logger = config.GetLogger("main")
+
+	err := config.Init()
+	if err != nil {
+		logger.Errorf("config initialization error: %v", err)
+		return 
+	}
 
 	router.Initialize()
-
 }
