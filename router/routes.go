7181f08d0b1a31eaad1d8627584fe40c87256e9e
lucasbezq <lucasbarrosez@gmail.com>
2024-11-15 14:54:05 -0300

    feat: extrair a estrutura do handler para um package próprio

--- router/routes.go
+++ router/routes.go
@@ -1,38 +1,17 @@
 package router
 
 import (
-	"net/http"
-
 	"github.com/gin-gonic/gin"
+	"github.com/lucasbezq/jobspot-api/handler"
 )
 
 func initializeRoutes(router *gin.Engine) {
 	v1 := router.Group("/api/v1")
 	{
-		v1.GET("/opennings/oppening", func(ctx *gin.Context) {
-			ctx.JSON(http.StatusOK, gin.H{
-				"message": "GET opennings",
-			})
-		})
-        v1.GET("/opennings", func(ctx *gin.Context) {
-			ctx.JSON(http.StatusOK, gin.H{
-				"message": "GET opennings",
-			})
-		})
-        v1.POST("/opennings", func(ctx *gin.Context) {
-			ctx.JSON(http.StatusOK, gin.H{
-				"message": "POST opennings",
-			})
-		})
-        v1.DELETE("/opennings", func(ctx *gin.Context) {
-			ctx.JSON(http.StatusOK, gin.H{
-				"message": "DELETE opennings",
-			})
-		})
-        v1.PUT("/opennings", func(ctx *gin.Context) {
-			ctx.JSON(http.StatusOK, gin.H{
-				"message": "PUT opennings",
-			})
-		})
+		v1.POST("/opennings/oppening", handler.CreateOppeningHandler)
+		v1.GET("/opennings", handler.FindOppeningHandler)
+		v1.PUT("/opennings", handler.UpdateOppeningHandler)
+		v1.DELETE("/opennings", handler.DeleteOppeningHandler)
+		v1.GET("/opennings", handler.ListOppeningHandler)
 	}
 }
