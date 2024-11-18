8c703490022f3dd35d45d0972f56bf7914d9ae69
lucasbezq <lucasbarrosez@gmail.com>
2024-11-18 20:52:18 -0300

    feat: adicionar regras para os handlers do CRUD de opening

--- handler/findOpeningHandler.go
+++ handler/findOpeningHandler.go
@@ -1,13 +1,27 @@
 package handler
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/lucasbezq/jobspot-api/schemas"
 )
 
 func FindOpeningHandler(ctx *gin.Context) {
-	ctx.JSON(http.StatusOK, gin.H{
-		"message": "GET opennings",
-	})
+	id := ctx.Query("id")
+	if id == "" {
+		sendError(ctx, http.StatusNotFound,
+			errParamIsRequired("id", "queryParameter").Error())
+		return
+	}
+
+	opening := schemas.Opening{}
+	if err := db.First(&opening, id).Error; err != nil {
+		sendError(ctx, http.StatusNotFound,
+			fmt.Sprintf("opening with id: %s not found", id))
+		return
+	}
+
+	sendSuccess(ctx, "find-opening", opening)
 }
